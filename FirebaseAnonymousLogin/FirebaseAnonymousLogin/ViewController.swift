53d40f800588025f2ac3e2a4b5a539f78fc6dd3b
長坂豪士 <nagasakatsuyoshi@nagasakagoushinoMacBook-Air.local>
2019-10-10 15:52:12 +0900

    first commit

---
+++ FirebaseAnonymousLogin/FirebaseAnonymousLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseAnonymousLogin
+//
+//  Created by 長坂豪士 on 2019/10/10.
+//  Copyright © 2019 Tsuyoshi Nagasaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
